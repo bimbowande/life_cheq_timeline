ce58a17e06cefa8d90c0ab9b90ea87ea892a04a3
bimbowande <bimbowande@gmail.com>
2022-07-19 05:00:29 +0100

    folder structure and initialization

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {HashRouter} from "react-router-dom"
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Routing} from './Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routing/>
+    </HashRouter>
   </React.StrictMode>
 );
 
